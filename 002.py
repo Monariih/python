8948800f0e82630a00505e74019952196d85e644
Monariih <102758346+Monariih@users.noreply.github.com>
2022-03-31 15:39:02 -0300

    Create 002.py

---
+++ 002.py
@@ -0,0 +1,5 @@
+nome=input('Qual é o seu nome?')
+dia=input('Qual dia você nasceu?')
+mes=input('Qual mes do seu nascimento?')
+ano=input('E em qual ano voce veio ao mundo?')
+print('Olá',nome,'!','Seja ben vindo ao meu curso de programação python, você nasceu no dia',dia,'do mes de',mes,'no ano de',ano,' estou correto?')
